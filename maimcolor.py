7fc8cfe7271feeab596e50a54f98c4ad80c144d1
kurtka823 <kurtka823@gmail.com>
2026-04-24 21:06:20 +0400

    first commit

---
+++ maimcolor.py
@@ -0,0 +1,14 @@
+import colorama
+
+# запуск библиотеки
+colorama.init()
+
+# примеры
+print(colorama.Fore.RED + "Красный текст")
+print(colorama.Fore.GREEN + "Зелёный текст")
+print(colorama.Style.BRIGHT + "Яркий текст")
+print(colorama.Style.RESET_ALL + "Обычный текст")
+
+# интроспекция 
+print(dir(colorama))
+help(colorama)
